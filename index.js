a5f9ab52edb62e027f4d084610708363a8a2acf1
BaniEmefa <82719086+BaniEmefa@users.noreply.github.com>
2021-07-22 08:19:34 +0000

    first upload

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
 const app = express()
+app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use('', appRoutes)
 
