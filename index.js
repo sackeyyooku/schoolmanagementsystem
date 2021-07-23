c9758b3e8d7bae3447e5a7f3af61f44e152ed414
Andrew <andrewfometi@gmail.com>
2021-07-23 14:45:40 +0100

    initail commit

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 require('dotenv').config()
 const express = require('express')
 const appRoutes = require('./routes')
-const bodyParser = require('body-parser')
+//const bodyParser = require('body-parser')
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
-
+//const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const app = express()
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use('', appRoutes)
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json()) // To parse the incoming requests with JSON payloads
 
 
 app.listen(process.env.PORT, ()=>{
